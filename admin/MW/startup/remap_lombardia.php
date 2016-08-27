846df60955b2e16d5ead0429419042786373a552
Joseph Rossetto <joseph.rossetto@gmail.com>
2016-08-27 16:32:19 +0100

    Added admin/MW/startup/remap_lombardia.php to remap admin locale file

---
+++ admin/MW/startup/remap_lombardia.php
@@ -0,0 +1,22 @@
+<?php 
+/* SVN FILE: $Id$ */
+
+/**
+ * @copyright Copyright(c) 2005-2009 Ministero per i beni e le attività culturali. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ *
+ * Museo & Web CMS is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ *
+ * @author		Daniele Ugoletti <daniele@ugoletti.com>, Gruppo Meta <http://www.gruppometa.it>
+ * @version		$Id: it.php,$
+ * @package		Museo&Web CMS
+ * @category	script
+ */
+ 
+	include('../MW/startup/remap_lombardia.php');
+	
+?>
